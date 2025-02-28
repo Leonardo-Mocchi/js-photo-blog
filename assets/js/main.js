8c8ae0c09393ed1660fe9f534fafc87c1bd64eba
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-02-28 15:33:18 +0100

    add milestone 3 & fix responsiveness DONE

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,79 @@
-/* .catch(error => console.error(error)); */
+// assign the DOM node of the container
+const postsEl = document.getElementById("posts_container")
+
+// create 6 posts
+for (let i = 0; i < 6; i++) {
+    //create posts by adding them as HTML
+    postsEl.innerHTML += addStructure()
+}
+
+// select the DOM node of the single cards created above
+const cardsEl = document.querySelectorAll(".my_card")
+console.log(cardsEl);
+
+// fetch the cards raw details
+fetch("https://lanciweb.github.io/demo/api/pictures/")
+    .then(response => response.json())
+    .then(data => {
+        // log the data to see what we have
+        console.log(data);
+
+        // show single posts using a loot  (through a loop) the posts value saving it in a variable
+        for (let i = 0; i < data.length; i++) {
+            const thisData = data[i];
+            console.log(thisData);
+
+            const thisCard = document.querySelectorAll(".my_card")
+            console.log(thisCard);
+
+            // assign the 4 data values to variables through destructuring
+            const [thisCardID, thisCardTitle, thisCardDate, thisCardImgURL] = [thisData.id, thisData.title, thisData.date, thisData.url]; //thisCardDetails
+            console.log(thisCardID, thisCardTitle, thisCardDate, thisCardImgURL);
+
+            // create a variable that adds a <p> HTML element inside
+            const cardsNode = document.createElement("div")
+            // make the new paragraph display the generated file
+            cardsNode.innerHTML = `
+                    <img src="${thisCardImgURL}" alt="">
+                    <p class="mt-3 mb-0 google_font_date">${thisCardDate}</p>
+                    <h4 class="m-0 google_font_title">${thisCardTitle.toUpperCase()}</h4>`;
+            // fix the margins of the new element
+            /* cardsNode.classList.add('m-0'); */
+
+
+            /* console.log(cardsNode); */
+
+            // officially insert the above variable inside every i° element of the HTML
+            thisCard[i].appendChild(cardsNode);
+        }
+
+    })/* .catch(error => console.error(error)); */
+
+
+
+// functions region
+
+
+/**
+ * Adds the structure of the blog posts
+ * @returns {string} HTML structure
+ */
+function addStructure() {
+    return `<div class="col">
+                <div class="my_card p-3 m-0">
+                    <img class="pin" src="./assets/img/pin.svg" alt="">
+                </div>
+            </div>`
+}
+
+/* function addCardsMarkup() {
+    return `<img src="${thisCardImgURL}" alt="">
+            <p class="">${thisCardDate}</p>
+            <h4>${thisCardTitle}</h4>`
+} */
+
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -1,8 +1,9 @@
 CONSEGNA
-Milestone 1
+
+//done// Milestone 1
 Sfruttando gli screen e gli asset in allegato riproduciamo la grafica proposta in maniera statica: utilizzando soltanto HTML e CSS e riproducendo una singola fotografia (usiamo una qualunque immagine a piacimento)
 
-Milestone 2
+//done// Milestone 2
 Utilizzando Postman, testiamo una chiamata a questo endpoint:
 https://lanciweb.github.io/demo/api/pictures/
 Studiamo bene la risposta e i dati che ci fornisce iniziando a pensare a come poterli sfruttare.
@@ -10,9 +11,9 @@ Studiamo bene la risposta e i dati che ci fornisce iniziando a pensare a come po
 Milestone 3
 Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API, sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
 
-Font utilizzati:
+//done// Font utilizzati:
 titoli: ‘Edu Tas Beginner’, sans-serif;
 date: ‘Sometype Mono’, ‘monospace’;
 
-Bonus
+//done// Bonus
 rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano man mano una sotto l’altra ed il titolo abbia una dimensione adeguata
