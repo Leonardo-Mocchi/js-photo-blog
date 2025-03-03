2889729f17c048415600d95cb2f20ec81a713086
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-03-03 15:26:52 +0100

    add functional overlays with test images, not correct

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,10 +18,6 @@ fetch("https://lanciweb.github.io/demo/api/pictures/")
         // log the data to see what we have
         /* console.log(data); */
 
-        const myPictures = []
-        console.log(myPictures);
-
-
         // show single posts using a loot  (through a loop) the posts value saving it in a variable
         for (let i = 0; i < data.length; i++) {
             const thisData = data[i];
@@ -48,10 +44,41 @@ fetch("https://lanciweb.github.io/demo/api/pictures/")
             // officially insert the above variable inside every i° element of the HTML
             thisCard[i].appendChild(cardsNode);
 
-            const normalPic = document.querySelectorAll(".my_normal_pic")
-            console.log(normalPic);
-            myPictures.push(normalPic)
         }
+
+        const normalPicEl = document.querySelectorAll(".my_normal_pic")
+        const closeOverlayEl = document.querySelectorAll(".close_my_overlay")
+        const overlayEl = document.querySelectorAll(".my_overlay")
+        const overlayPicEl = document.querySelectorAll(".my_overlay_pic")
+        console.log(normalPicEl, closeOverlayEl, overlayEl, overlayPicEl);
+
+        picOverlayOpen(normalPicEl)
+
+        function picOverlayOpen(arr) {
+
+            for (let i = 0; i < arr.length; i++) {
+                const thisPic = arr[i];
+                console.log(thisPic);
+
+                thisPic.addEventListener("click", () => {
+                    overlayEl[i].classList.remove("d-none")
+                })
+            }
+        }
+
+        picOverlayClose(closeOverlayEl)
+
+        function picOverlayClose(arr) {
+
+            for (let i = 0; i < arr.length; i++) {
+                const thisCloseButton = arr[i];
+
+                thisCloseButton.addEventListener("click", () => {
+                    overlayEl[i].classList.add("d-none")
+                })
+            }
+        }
+
     }).catch(error => console.error(error));
 
 
@@ -59,42 +86,12 @@ fetch("https://lanciweb.github.io/demo/api/pictures/")
 
 addOverlays(cardsEl)
 
-const closeOverlayEl = document.querySelectorAll(".close_my_overlay")
-const overlayEl = document.querySelectorAll(".my_overlay")
-const overlayPicEl = document.querySelectorAll(".my_overlay_pic")
-/* const normalPicEl = document.querySelectorAll(".my_normal_pic") */
-/* console.log(closeOverlayEl, overlayEl, overlayPicEl, normalPicEl); */
-/* console.log(normalPicEl); */
 
 
 
-/* picOverlayOpen(normalPicEl)
 
-function picOverlayOpen(arr) {
 
-    for (let i = 0; i < arr.length; i++) {
-        const thisPic = arr[i];
-        console.log(thisPic);
 
-        thisPic.addEventListener("click", () => {
-            overlayEl[i].classList.remove("d-none")
-            
-        })
-    }
-} */
-
-/* picOverlayClose(closeOverlayEl)
-
-function picOverlayClose(arr) {
-
-    for (let i = 0; i < arr.length; i++) {
-        const thisCloseButton = arr[i];
-
-        thisCloseButton.addEventListener("click", () => {
-            overlayEl.classList.add("d-none")
-        })
-    }
-} */
 
 // functions region
 
